589b3cb373eca3a190c19db2b6cf19889b16e17e
Christian de Botton <cdebotton@gmail.com>
2013-01-23 17:19:50 -0500

    Switching from lodash to underscore.
    Signed-off-by: Christian de Botton <cdebotton@gmail.com>

--- backbone-relational.js
+++ backbone-relational.js
@@ -1,24 +1,28 @@
-/* vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab: */
 /**
- * Backbone-relational.js 0.7.1
- * (c) 2011-2013 Paul Uithol and contributors (https://github.com/PaulUithol/Backbone-relational/graphs/contributors)
+ * Backbone-relational.js 0.6.0
+ * ----- AMD Fork by Dexnode -----
+ * (c) 2011 Paul Uithol
  *
  * Backbone-relational may be freely distributed under the MIT license; see the accompanying LICENSE.txt.
  * For details and documentation: https://github.com/PaulUithol/Backbone-relational.
  * Depends on Backbone (and thus on Underscore as well): https://github.com/documentcloud/backbone.
  */
+
 (function(root, factory) {
+  // Set up appropriately for the environment.
   if (typeof exports !== 'undefined') {
-    factory(root, exports, require('lodash'), require('backbone'));
+    // Node/CommonJS
+    factory(root, exports, require('underscore'), require('backbone'));
   } else if (typeof define === 'function' && define.amd) {
-    define('backbone-relational', ['lodash', 'backbone', 'exports'], function(_, Backbone, exports) {
-      return factory(root, exports, _, Backbone);
+    // AMD
+    define('backbone-relational', ['underscore', 'backbone', 'exports'], function(_, Backbone, exports) {
+      factory(root, exports, _, Backbone);
     });
   } else {
+    // Browser globals
     factory(root, {}, root._, root.Backbone);
   }
 }(this, function(root, exports, _, Backbone) {
-	"use strict";
 
 	Backbone.Relational = {
 		showWarnings: true
@@ -1730,5 +1734,4 @@
 		return child;
 	};
 
-	return Backbone;
 }));
